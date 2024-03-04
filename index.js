bd80e2789edc24e11e239384b1ad65b0f14fe3a8
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-03-04 12:48:06 -0300

    Done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos]
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length != 0) {
+      return this.items[this.length - 1]
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length != 0) {
+      return this.items[0]
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((total, num) => total + num, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length != 0) {
+      return this.sum() / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
